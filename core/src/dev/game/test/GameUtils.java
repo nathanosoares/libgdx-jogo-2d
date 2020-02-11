ed6b824ae9849637604139ef8d2a6c4dcc1cae21
Nathan Soares <eu@nathan.com.br>
2020-02-11 15:30:29 -0300

    development

--- core/src/dev/game/test/GameUtils.java
+++ core/src/dev/game/test/GameUtils.java
@@ -2,6 +2,7 @@ package dev.game.test;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.math.Vector3;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -16,5 +17,15 @@ public class GameUtils {
         Gdx.gl.glClearColor(red / 255f, green / 255f, blue / 255f, alpha / 255f);
 
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    public static Vector3 cartesianToIsometric(Vector3 vector) {
+        Vector3 out = new Vector3();
+
+        out.x = vector.x - vector.z;
+        out.y = vector.y;
+        out.z = (vector.x + vector.z) / 2;
+
+        return out;
     }
 }
