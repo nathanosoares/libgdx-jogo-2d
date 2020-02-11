96db0bbc40907339accdde294fdbe9677d3fa06c
Ricardo Falcão <ricardomanuelfalcao20@gmail.com>
2020-02-11 12:04:13 +0000

    Adicionado lombok e organizado gradle

--- desktop/src/dev/game/test/desktop/DesktopLauncher.java
+++ desktop/src/dev/game/test/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import dev.game.test.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.forceExit = false;
 		new LwjglApplication(new Game(), config);
 	}
 }
