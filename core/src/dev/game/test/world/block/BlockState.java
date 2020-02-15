795509bd76ae03357f5a26c1b8d16eac504d4459
Nathan Soares <eu@nathan.com.br>
2020-02-15 17:20:01 -0300

    development

--- core/src/dev/game/test/world/block/BlockState.java
+++ core/src/dev/game/test/world/block/BlockState.java
@@ -1,8 +1,12 @@
 package dev.game.test.world.block;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import dev.game.test.world.World;
 import dev.game.test.world.WorldLayer;
+import dev.game.test.world.block.impl.BlockStone;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,32 +14,42 @@ import lombok.Setter;
 @Getter
 public class BlockState {
 
-    @Setter
     private Block block;
 
-
-    //
-
     private World world;
-
     private WorldLayer layer;
-
-    //
 
     private Vector2 position;
 
-    //
-
     public int connectedData;
-
-    /*
-
-     */
 
     public BlockState(Block block, World world, WorldLayer layer, Vector2 position) {
         this.block = block;
         this.world = world;
         this.layer = layer;
         this.position = position;
+    }
+
+    Body body;
+
+    public void setBlock(Block block) {
+        this.block = block;
+
+        if (block instanceof BlockStone) {
+            if (this.body != null) {
+                world.getBox2dWorld().destroyBody(this.body);
+            }
+
+            BodyDef def = new BodyDef();
+            def.position.set(this.position);
+            this.body = world.getBox2dWorld().createBody(def);
+
+            PolygonShape groundBox = new PolygonShape();
+            groundBox.setAsBox(18f / 2, 18f / 2);
+
+            this.body.createFixture(groundBox, 0f);
+
+            groundBox.dispose();
+        }
     }
 }
