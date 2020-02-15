028c0807b9ad6e5683cdc93cbc30eb77abbb7d9a
Ricardo Falcão <ricardomanuelfalcao20@gmail.com>
2020-02-15 15:42:59 +0000

    Sistema de texturas ligadas

---
+++ core/src/dev/game/test/world/block/BlockAir.java
@@ -0,0 +1,15 @@
+package dev.game.test.world.block;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import dev.game.test.world.block.connected.EnumTextureConnection;
+
+public class BlockAir extends Block {
+
+    public BlockAir() {
+
+    }
+
+}
