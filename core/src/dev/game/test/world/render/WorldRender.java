63ca66dd044c2c2e23aaa7b61a326b5b6dff04b2
Nathan Soares <eu@nathan.com.br>
2020-02-15 09:13:09 -0300

    development

--- core/src/dev/game/test/world/render/WorldRender.java
+++ core/src/dev/game/test/world/render/WorldRender.java
@@ -5,15 +5,13 @@ import com.badlogic.gdx.utils.Disposable;
 import dev.game.test.world.World;
 import dev.game.test.world.WorldLayer;
 import dev.game.test.world.block.BlockData;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class WorldRender implements Disposable {
 
-    protected Batch batch;
-    protected World world;
-
-    private WorldRender(World world) {
-
-    }
+    protected final Batch batch;
+    protected final World world;
 
     public void render() {
         beginRender();
@@ -21,7 +19,7 @@ public class WorldRender implements Disposable {
         for (WorldLayer layer : world.getLayers()) {
             for (BlockData[] blocks : layer.getBlocks()) {
                 for (BlockData block : blocks) {
-                    block.getBlock().getTexture(block);
+                    this.batch.draw(block.getBlock().getTexture(block), block.getPosition().x, block.getPosition().y);
                 }
             }
         }
