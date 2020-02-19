f06fbc8cd0f67acd40cf5338b423ce083536e861
Ricardo Falcão <ricardomanuelfalcao20@gmail.com>
2020-02-19 22:07:28 +0000

    Update GameScreen.java

--- client/src/dev/game/test/client/screens/GameScreen.java
+++ client/src/dev/game/test/client/screens/GameScreen.java
@@ -1,6 +1,6 @@
 package dev.game.test.client.screens;
 
-import com.artemis.WorldConfigurationBuilder;
+import com.artemis.WorldConfiguration;
 import com.artemis.link.EntityLinkManager;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Graphics.DisplayMode;
@@ -15,8 +15,8 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.google.common.collect.Maps;
-import dev.game.test.client.ClientConstants;
 import dev.game.test.client.ClientApplication;
+import dev.game.test.client.ClientConstants;
 import dev.game.test.client.GameUtils;
 import dev.game.test.client.entity.components.SpriteComponent;
 import dev.game.test.client.entity.systems.CollidableDebugSystem;
@@ -24,14 +24,14 @@ import dev.game.test.client.entity.systems.PlayerControllerSystem;
 import dev.game.test.client.entity.systems.SpriteRenderSystem;
 import dev.game.test.client.world.WorldClient;
 import dev.game.test.client.world.systems.WorldRenderSystem;
+import dev.game.test.core.Injection;
 import dev.game.test.core.entity.EntityFactory;
 import dev.game.test.core.entity.components.TransformComponent;
 import dev.game.test.core.entity.systems.MovementSystem;
+import java.util.Map;
 import lombok.Getter;
 import net.namekdev.entity_tracker.EntityTracker;
 import net.namekdev.entity_tracker.ui.EntityTrackerMainWindow;
-
-import java.util.Map;
 
 @Getter
 public class GameScreen extends ScreenAdapter {
@@ -72,10 +72,10 @@ public class GameScreen extends ScreenAdapter {
     public void show() {
         EntityFactory.postCreate = (world, entityId) -> {
             world.edit(entityId)
-                    .add(new SpriteComponent(new Sprite(
-                            new Texture("rpg-pack/chars/gabe/gabe-idle-run.png"),
-                            5, 2, 16, 22
-                    )));
+                .add(new SpriteComponent(new Sprite(
+                    new Texture("rpg-pack/chars/gabe/gabe-idle-run.png"),
+                    5, 2, 16, 22
+                )));
         };
 
         this.camera = new OrthographicCamera();
@@ -92,21 +92,24 @@ public class GameScreen extends ScreenAdapter {
 
         WorldClient world = new WorldClient("world", mapWidth, mapHeight);
 
-        WorldConfigurationBuilder builder = new WorldConfigurationBuilder()
-                .with(new EntityLinkManager())
-                .with(new MovementSystem())
-                .with(new PlayerControllerSystem(this))
-                .with(new WorldRenderSystem(world, this.camera, this.spriteBatch, this.viewport))
-                .with(new SpriteRenderSystem(this.spriteBatch));
+        WorldConfiguration worldConfiguration = new WorldConfiguration();
+
+        worldConfiguration.setSystem(new EntityLinkManager());
+        worldConfiguration.setSystem(new MovementSystem());
+        worldConfiguration.setSystem(new PlayerControllerSystem(this));
+        worldConfiguration.setSystem(new WorldRenderSystem(world, this.camera, this.spriteBatch, this.viewport));
+        worldConfiguration.setSystem(new SpriteRenderSystem(this.spriteBatch));
+
+        worldConfiguration = Injection.injectSingletons(worldConfiguration);
 
         if (ClientConstants.DEBUG) {
             this.entityTrackerMainWindow = new EntityTrackerMainWindow(false, false);
 
-            builder.with(new EntityTracker(entityTrackerMainWindow))
-                    .with(new CollidableDebugSystem(this.spriteBatch));
+            worldConfiguration.setSystem(new EntityTracker(entityTrackerMainWindow));
+            worldConfiguration.setSystem(new CollidableDebugSystem(this.spriteBatch));
         }
 
-        this.artemis = new com.artemis.World(builder.build());
+        this.artemis = new com.artemis.World(worldConfiguration);
 
         this.artemis.inject(entityFactory);
 
@@ -130,19 +133,23 @@ public class GameScreen extends ScreenAdapter {
         GameUtils.clearScreen(0, 50, 0, 100);
 
         TransformComponent transformComponent = this.artemis.getEntity(playerId)
-                .getComponent(TransformComponent.class);
+            .getComponent(TransformComponent.class);
 
         if (transformComponent != null) {
             this.camera.position.set(transformComponent.position.x, transformComponent.position.y, 0);
 
-            float visibleW = viewport.getWorldWidth() / 2.0f + (float) viewport.getScreenX() / (float) viewport.getScreenWidth() * viewport.getWorldWidth();//half of world visible
-            float visibleH = viewport.getWorldHeight() / 2.0f + (float) viewport.getScreenY() / (float) viewport.getScreenHeight() * viewport.getWorldHeight();
-
+            float visibleW = viewport.getWorldWidth() / 2.0f + (float) viewport.getScreenX() / (float) viewport.getScreenWidth() * viewport
+                .getWorldWidth();//half of world visible
+            float visibleH =
+                viewport.getWorldHeight() / 2.0f + (float) viewport.getScreenY() / (float) viewport.getScreenHeight() * viewport
+                    .getWorldHeight();
 
             WorldRenderSystem renderSystem = this.artemis.getSystem(WorldRenderSystem.class);
 
-            this.camera.position.x = MathUtils.clamp(this.camera.position.x, visibleW, renderSystem.getWorldClient().getBounds().getWidth() - visibleW);
-            this.camera.position.y = MathUtils.clamp(this.camera.position.y, visibleH, renderSystem.getWorldClient().getBounds().getHeight() - visibleH);
+            this.camera.position.x = MathUtils
+                .clamp(this.camera.position.x, visibleW, renderSystem.getWorldClient().getBounds().getWidth() - visibleW);
+            this.camera.position.y = MathUtils
+                .clamp(this.camera.position.y, visibleH, renderSystem.getWorldClient().getBounds().getHeight() - visibleH);
             this.camera.update();
         }
 
