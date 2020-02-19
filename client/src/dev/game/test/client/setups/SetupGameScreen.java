ce21c7879120ca8d0cdff33a9147f10feac352a4
Nathan Soares <eu@nathan.com.br>
2020-02-19 19:19:10 -0300

    development

--- client/src/dev/game/test/client/setups/SetupGameScreen.java
+++ client/src/dev/game/test/client/setups/SetupGameScreen.java
@@ -3,23 +3,17 @@ package dev.game.test.client.setups;
 import com.artemis.annotations.Wire;
 import dev.game.test.client.ClientApplication;
 import dev.game.test.client.screens.GameScreen;
-import dev.game.test.core.GameApplication;
 import dev.game.test.core.setup.Setup;
 
 public class SetupGameScreen implements Setup {
 
     @Wire
-    private GameApplication application;
+    private ClientApplication application;
 
     @Override
     public void setup() {
 
-        ClientApplication clientApplication = (ClientApplication) application;
-
-        int mapWidth = 20;
-        int mapHeight = 20;
-
-        GameScreen screenGame = new GameScreen(clientApplication);
-        clientApplication.getScreenManager().setCurrentScreen(screenGame);
+        GameScreen screenGame = new GameScreen(application);
+        application.getScreenManager().setCurrentScreen(screenGame);
     }
 }
