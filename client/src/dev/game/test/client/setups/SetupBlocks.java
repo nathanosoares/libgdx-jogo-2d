4fd67aa2b7b272dcb7ad3d303ced4ba3b6495dbf
Nathan Soares <eu@nathan.com.br>
2020-02-19 18:58:59 -0300

    development

--- client/src/dev/game/test/client/setups/SetupBlocks.java
+++ client/src/dev/game/test/client/setups/SetupBlocks.java
@@ -1,16 +1,21 @@
 package dev.game.test.client.setups;
 
+import com.artemis.annotations.Wire;
 import dev.game.test.client.block.BlockClient;
 import dev.game.test.client.block.Blocks;
 import dev.game.test.client.registry.RegistryBlocks;
 import dev.game.test.core.GameApplication;
+import dev.game.test.core.registry.RegistryManager;
 import dev.game.test.core.setup.Setup;
 
 public class SetupBlocks implements Setup {
 
+    @Wire
+    private RegistryManager registryManager;
+
     @Override
     public void setup(GameApplication application) {
-        RegistryBlocks registryBlocks = application.getRegistryManager().getRegistry(BlockClient.class);
+        RegistryBlocks registryBlocks = registryManager.getRegistry(BlockClient.class);
 
         registryBlocks.registerBlock(0, Blocks.AIR);
         registryBlocks.registerBlock(1, Blocks.DIRT);
