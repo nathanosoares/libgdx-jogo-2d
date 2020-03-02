f033d6f4a1fad65ea1e7756df332c95efbcb7891
Nathan Soares <eu@nathan.com.br>
2020-03-02 10:31:09 -0300

    fix: stone texture

--- core/src/dev/game/test/core/block/impl/BlockStone.java
+++ core/src/dev/game/test/core/block/impl/BlockStone.java
@@ -1,7 +1,25 @@
 package dev.game.test.core.block.impl;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import dev.game.test.api.block.IBlockState;
 import dev.game.test.core.block.Block;
 
 public class BlockStone extends Block {
 
+    private TextureRegion texture;
+
+    @Override
+    public void loadTextures() {
+        this.texture = new TextureRegion(new Texture(Gdx.files.internal("map/stone.png")));
+    }
+
+
+    @Override
+    public TextureRegion getTexture(IBlockState blockState) {
+        return texture;
+    }
 }
